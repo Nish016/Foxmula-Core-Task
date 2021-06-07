9594040df745eee5d7c2846f736b9427c63e4872
Nishkarsh <dahiyanishkarsh@gmail.com>
2021-06-07 18:37:25 +0530

    Core Task completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import Column from "./Components/Column";
+import {FaAddressCard , FaRegEdit, FaClock} from "react-icons/fa";
+import {MdPersonAdd , MdPerson} from "react-icons/md";
 
 function App() {
   let data = [
@@ -216,11 +218,51 @@ function App() {
     },
   ];
   return (
+    <>
+    <div className = "i_card">
+
+      <div className = "batch"> 
+        <h4>Batch Name | PHY_B1</h4>
+        <FaRegEdit />
+      </div>
+
+      <div className = "cardOne">
+        <FaAddressCard />
+        <p>Nishkarsh Dahiya</p>
+        <MdPersonAdd size = "24px"/>
+      </div>
+
+      
+      <div className = "cardTwo">
+        <MdPerson />
+        <p>125 students</p>
+      </div>
+
+      <div className = "cardThree">
+        <FaClock />
+        <div className = "activity">
+          <p>Upcoming Activity</p>
+          <p>Thu, 25th May</p>
+          <h4>12:00 PM - 12:00 PM</h4>
+        </div>
+
+        <button className = "button">View</button>
+      </div>
+
+
+    </div>
+
+
+
+
+
+
     <div className={"table"}>
       {data.map((item, index) => (
         <Column key={index} data={item} />
       ))}
     </div>
+    </>
   );
 }
 export default App;
