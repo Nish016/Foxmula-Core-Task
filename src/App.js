32ab91dd3d32734e93ab6fba6f409cb2b666ffe1
Nishkarsh <dahiyanishkarsh@gmail.com>
2021-06-07 16:48:33 +0530

    Core Task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,226 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Column from "./Components/Column";
 
 function App() {
+  let data = [
+    {
+      title: "MON",
+      data: [
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "Due at 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+      ],
+    },
+    {
+      title: "TUE",
+      data: [
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+      ],
+    },
+    {
+      title: "WED",
+      data: [
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "Due at 12:00 AM",
+        },
+
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "Due at 12:00 AM",
+        },
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "Due at 12:00 AM",
+        },
+      ],
+    },
+    {
+      title: "THU",
+      data: [
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+      ],
+    },
+    {
+      title: "FRI",
+      data: [
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "Due at 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+      ],
+    },
+    {
+      title: "SAT",
+      data: [
+        {
+          title: "Homework",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "Due at 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+      ],
+    },
+    {
+      title: "SUN",
+      data: [
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Class",
+          time: "12:00 AM - 12:00 AM",
+        },
+
+        {
+          title: "Test",
+          head: "Lorem_Lorem_Lorem",
+          description: "Lorem_Lorem_Lorem",
+          time: "12:00 AM - 12:00 AM",
+        },
+      ],
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"table"}>
+      {data.map((item, index) => (
+        <Column key={index} data={item} />
+      ))}
     </div>
   );
 }
-
 export default App;
